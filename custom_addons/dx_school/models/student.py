2a683579e01ac74eb3cbd3fcba7c920f1b64481f
Arnav Gupta <info@arnavgupta.net>
2024-12-27 16:20:25 +0530

    refactor: deleted uneccesary files

--- custom_addons/dx_school/models/student.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-class SchoolStudent(models.Model):
-    _name = 'school.student'
-    _description = 'Students in the school'
-    _inherit = ['mail.thread']
-    name = fields.Char('Name', required=True, tracking=True)
-    date_of_birth = fields.Date('Date of Birth')
